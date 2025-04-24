f1ae0f6e131bfc752fcb43594c8641ae293f7d7f
josephsurrey <josephsurrey2018@gmail.com>
2025-04-24 19:47:42 +1200

    Added constants to constants.py
    Some values have been left blank until they can be tested to work out the correct values.

---
+++ constants.py
@@ -0,0 +1,22 @@
+WINDOW_WIDTH = 900 # Width of the game window
+WINDOW_HEIGHT = 400 # Height of the game window
+FPS = 30 # Frames per second
+
+GROUND_Y = 100 # Y position of the ground
+
+GRAVITY = 0 # Acceleration due to gravity
+JUMP_SPEED = 0 # Speed at which the player jumps
+
+PLAYER_HORIZONTAL_POSITION = 100 # Starting horizontal position of the player
+
+OBSTACLE_INITIAL_SPEED = 0 # Initial speed of obstacles
+OBSTACLE_CREATION_INTERVAL = 2000 # Time interval between obstacle creations
+# (in milliseconds)
+
+# Standard colour values
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREY = (128, 128, 128)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
